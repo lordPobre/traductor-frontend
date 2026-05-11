d4f69a9787c7642d70c01c833671ff455ce9c261
Carlos Lopez <clopez.ti@vcchile.cl>
2026-05-10 20:44:20 -0400

    Limpieza de carpetas anidadas por accidente

--- backend/translator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_document, name='upload_document'),
-    path('status/<uuid:document_id>/', views.get_status, name='get_status'), # <-- AÑADIR ESTA
-]
